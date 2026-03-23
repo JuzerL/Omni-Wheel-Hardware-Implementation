32551cc72dc6d7246787fe701bbe7444a1a1ede1
JuzerL <juzerrezuj@gmail.com>
2026-03-23 22:14:47 +0530

    Update and rename omni_hardware.cpp to omni_hardware.hpp

---
+++ robot_hardware/include/omni_hardware.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "rclcpp/macros.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#include <vector>
+#include <string>
+
+namespace robot_hardware
+{
+
+class OmniHardware : public hardware_interface::SystemInterface
+{
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(OmniHardware)
+
+  hardware_interface::CallbackReturn on_init(
+    const hardware_interface::HardwareInfo & info) override;
+
+  hardware_interface::CallbackReturn on_configure(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  hardware_interface::return_type read(
+    const rclcpp::Time & time,
+    const rclcpp::Duration & period) override;
+
+  hardware_interface::return_type write(
+    const rclcpp::Time & time,
+    const rclcpp::Duration & period) override;
+
+private:
+  std::vector<double> hw_commands_;
+  std::vector<double> hw_states_;
+};
+
+}  // namespace robot_hardware
